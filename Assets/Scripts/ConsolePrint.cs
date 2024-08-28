6d4f726345097a9e4f1ae197a457a3539a33b565
Jie <jie0403t0r.@gmail.com>
2024-08-28 23:45:49 +1000

    “80 percent complete

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    private int i = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i += 1;
+        //Debug.Log($"{gameObject.name}:{i} (from start)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (gameObject != null)
+        
+            Debug.Log($"{gameObject.name}:{i}");
+            i += 1;
+        
+        //else 
+        
+           // Debug.LogError("gameObject is null");
+        
+        
+    }
+}
